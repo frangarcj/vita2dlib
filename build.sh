abe098ffe874936a8b53807c24f3fea17c324952
Andre Brait <andrebrait@gmail.com>
2020-08-07 16:13:32 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z "${VITASDK+x}" ]; then
+    export VITASDK=/usr/local/vitasdk
+    export PATH=$VITASDK/bin:$PATH
+fi
+
+make -C libvita2d clean
+make -C libvita2d
+make -C libvita2d install
